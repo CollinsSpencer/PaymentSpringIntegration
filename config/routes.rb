396ef7cf02440c8aeed46e008b23d669ec434440
16jmiller <16jamill16@gmail.com>
2018-10-23 16:43:32 -0500

    more confirmation jazz

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get  '/checkout', to: 'pages#checkout'
   post '/single_payment', to: 'pages#single'
   post '/subscribe_payment', to: 'pages#subscribe'
-  get  '/confirmation', to 'pages#confirmation'
+  get  '/confirmation', to: 'pages#confirmation'
   root 'pages#welcome'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
